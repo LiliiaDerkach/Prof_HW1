19889ca7a01bff488b5f32d4cfb0364eafd5e82c
Liliia <derkach.lilia.iv@gmail.com>
2023-08-09 08:57:48 +0300

    First commit

---
+++ Prof_HW_1/MyCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public enum Month
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        November,
+        December
+    }
+    public class MyCollection
+    {
+        string[] _nameOfMonth = new string[]
+        {
+
+
+         };
+    }
+}
